b7dff2343c7665f65fc9385fcc50abe631b388e9
CMitchell1103 <mitch3cd@mail.uc.edu>
2024-09-30 22:49:07 -0400

    Final commit and push

---
+++ src/Main.java
@@ -0,0 +1,55 @@
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int intOperandA = 23;
+        int intOperandB = 24;
+
+        int intSum = 0;
+        int intQuotient = 0;
+        int intProduct = 0;
+        int intDifference = 0;
+        int intModulo = 0;
+
+        intSum = intOperandA + intOperandB;
+        System.out.println("The sum of " + intOperandA + " and " + intOperandB + " is " + intSum);
+        intQuotient = intOperandA / intOperandB;
+        System.out.println("The quotient of " + intOperandA + " and " + intOperandB + " is " + intQuotient );
+        intProduct = intOperandA * intOperandB;
+        System.out.println("The product of " + intOperandA + " and " + intOperandB + " is " + intProduct);
+        intDifference = intOperandA - intOperandB;
+        System.out.println("The difference of " + intOperandA + " and " + intOperandB + " is " + intDifference);
+        intModulo = intOperandA % intOperandB;
+        System.out.println("The modulo of " + intOperandA + " and " + intOperandB + " is " + intModulo);
+
+
+        double doubleOperandA = 23.23;
+        double doubleOperandB = 13.75;
+
+        double doubleSum = 0;
+        double doubleQuotient = 0;
+        double doubleProduct = 0;
+        double doubleDifference = 0;
+        double doubleModulo = 0;
+
+        doubleSum = doubleOperandA + doubleOperandB;
+        System.out.println("The sum of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleSum);
+        doubleQuotient = doubleOperandA / doubleOperandB;
+        System.out.println("The quotient of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleQuotient );
+        doubleProduct = doubleOperandA * doubleOperandB;
+        System.out.println("The product of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleProduct);
+        doubleDifference = doubleOperandA - doubleOperandB;
+        System.out.println("The difference of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleDifference);
+        doubleModulo = doubleOperandA % doubleOperandB;
+        System.out.println("The modulo of " + doubleOperandA + " and " + doubleOperandB + " is " + doubleModulo);
+
+
+
+
+
+
+
+
+
+    }
+}
